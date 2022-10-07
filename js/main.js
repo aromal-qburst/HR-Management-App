68866033e42a5eb8001368cb8695b633df9dbcaf
Aromal <aromal@qburst.com>
2022-10-07 17:36:12 +0530

    [WIP] Add employee skill chips

--- js/main.js
+++ js/main.js
@@ -149,6 +149,7 @@ const addUpdateModal = function (isAdd, empId) { // Handle modal on update or ad
     const modalBackground = document.getElementById('modal-background');
     const modalContent = modalBackground.querySelector('#addUpdate-emp');
     const modalHeading = modalContent.querySelector('#modal-heading');
+    const skillInput = modalContent.querySelector('#skill-display-section')
     const formSubmit = modalContent.querySelector('#form-submit');
     const formCancel = modalContent.querySelector('#form-cancel');
 
@@ -175,6 +176,8 @@ const addUpdateModal = function (isAdd, empId) { // Handle modal on update or ad
             }
         });
     };
+
+    autoCompleteSkill(skillInput);
 
     if (!isAdd) {
         fillUpdateClearForm(false, empId);
@@ -435,3 +438,115 @@ const filterEmployeeData = function (dropdownBox) { // Function filter skill, lo
 };
 
 /*-------END: Filter Employee Data Listing Implementation-------*/
+
+/*-------START: Autocomplete Skill Listing Implementation-------*/
+
+const autoCompleteSkill = function (skillInput) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+
+    let currentFocus;
+    const skillNames = JSON.parse(localStorage.getItem('skillData')).map(skillObj => skillObj.skillName);
+    /*execute a function when someone writes in the text field:*/
+    skillInput.addEventListener("input", function(e) {
+        let a, b, i;
+        let val = this.innerHTML.split('>').pop();
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < skillNames.length; i++) {
+          /*check if the item starts with the same letters as the text field value:*/
+          if (skillNames[i].slice(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + skillNames[i].slice(0, val.length) + "</strong>";
+            b.innerHTML += skillNames[i].slice(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + skillNames[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+            b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                let textEnteredLength = val.length;
+                skillInput.innerHTML = skillInput.innerHTML.slice(0, -textEnteredLength);
+                skillInput.innerHTML += `
+                <div class="chip" contenteditable="false">
+                <span class="skill-heading">${this.querySelector('input').value}</span>
+                <span class="skill-closebtn" onclick="this.parentElement.remove()">×</span>
+                </div>
+                `;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+                skillInput.focus();
+            });
+            a.appendChild(b);
+          }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    skillInput.addEventListener("keydown", function(e) {
+        let x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          /*If the arrow DOWN key is pressed,
+          increase the currentFocus variable:*/
+          currentFocus++;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 38) { //up
+          /*If the arrow UP key is pressed,
+          decrease the currentFocus variable:*/
+          currentFocus--;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          /*If the ENTER key is pressed, prevent the form from being submitted,*/
+          e.preventDefault();
+          if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (x) x[currentFocus].click();
+          }
+        }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (let i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      let x = document.getElementsByClassName("autocomplete-items");
+      for (let i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != skillInput) {
+          x[i].parentNode.removeChild(x[i]);
+        }
+      }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+  }
+
+/*-------END: Autocomplete Skill Listing Implementation-------*/
